1422dc70ba91376a0ef7b52b80335dd74b089807
ure <uy23@naver.com>
2023-03-13 16:25:55 +0900

    stream pagination

--- pages/api/streams/index.ts
+++ pages/api/streams/index.ts
@@ -29,7 +29,12 @@ async function handler(
       stream,
     })
   } else if (req.method === 'GET') {
-    const streams = await client.stream.findMany()
+    const streams = await client.stream.findMany({
+      // pagination 시 api 10개만 받아오고 20개를 건너뛰고 싶을때.
+      // 20개를 건너뛴 것은 3페이지일 때 (2page * 10) 스킵 후 다음 3page의 10개를 받아오게 하는것.
+      take: 10,
+      //   skip: 20,
+    })
     res.json({
       ok: true,
       streams,
