f280c640690e14bee5fc332eeddb1723fe9cae91
ure <uy23@naver.com>
2023-03-18 01:14:09 +0900

    use middleware

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -8,6 +8,7 @@ interface ProfileResponse {
   profile: User
 }
 
+// middleware 사용으로 인해 필요 없어진 코드인듯. 페이지 로드 되기 전에 미들웨어에서 처리하고 있음.
 export default function useUser() {
   const { data, error } = useSWR<ProfileResponse>('/api/users/me')
   const router = useRouter()
